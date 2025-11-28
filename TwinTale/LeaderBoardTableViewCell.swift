fb1d034130f07cb66c4337da8e183867f5ad6f66
Bhoj <gcbhoj@gmail.com>
2025-11-28 16:44:49 +0000

    bottom navbar pages complete, scoreboard complete. Deleted game page to redo it

---
+++ TwinTale/LeaderBoardTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LeaderBoardTableViewCell.swift
+//  TwinTale
+//
+//  Created by Default User on 11/28/25.
+//
+
+import UIKit
+
+class LeaderBoardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
